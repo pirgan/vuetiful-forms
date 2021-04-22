de6e3f667f5f25a957aebb52dc93f37f767b8bff
pirgan <pirgan@gmail.com>
2021-04-22 20:12:35 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container py-4">
+    <div class="row">
+      <div class="col-12">
+        <form v-on:submit.prevent="onSubmit">
+          <div class="form-group">
+            <label>First Name:</label>
+            <input v-model="form.firstName" type="text" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>Last Name:</label>
+            <input v-model="form.lastName" type="text" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>Email:</label>
+            <input v-model="form.email" type="email" class="form-control">
+          </div>
+          <div class="form-group">
+            <button v-bind:disabled="!formIsValid" type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      form: {
+        firstName: '',
+        lastName: '',
+        email: ''
+      }
+    }
+  },
+  computed: {
+    formIsValid() {
+      return this.form.firstName.length > 0 && this.form.lastName.length > 0 && this.form.email.length > 0;
+    }
+  },
+  methods: {
+    onSubmit() {
+			if (!this.formIsValid) return;
+			axios
+				.post('http://localhost:3000/dolphins', { params: this.form })
+				.then(response =>   {
+					console.log('Form has been posted', response);
+				}).catch(err => {
+					console.log('An error occurred', err);
+				});
+		}
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
